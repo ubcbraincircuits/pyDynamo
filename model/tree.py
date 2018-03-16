9c7739e377d5887bc29e0efa1f06ba5e770004de
padster <padsterpat@gmail.com>
2018-03-16 14:13:27 -0700

    A bunch of model documentation added

--- model/tree.py
+++ model/tree.py
@@ -29,10 +29,12 @@ class Point():
     """Not sure...? Isn't used... """
 
     def isRoot(self):
+        """Whether this point represents the root of the whole tree."""
         return self.parentBranch is None
 
     def indexInParent(self):
-        return self.parentBranch.points.index(self)
+        # """How far along the branch this point sits, 0 = first point after branch point."""
+        return self.parentBranch.indexForPoint(self)
 
 @attr.s
 class Branch():
@@ -57,37 +59,70 @@ class Branch():
     """Not sure...? Isn't used... """
 
     def indexInParent(self):
+        """Ordinal number of branch within the tree it is owned by."""
         return self._parentTree.branches.index(self)
 
     def indexForPoint(self, pointTarget):
+        """Given a point, return how far along the branch it sits."""
         for idx, point in enumerate(self.points):
             if point.id == pointTarget.id:
                 return idx
         return -1
 
+    def hasChildren(self):
+        """True if any point on the branch has child branches coming off it."""
+        return _lastPointWithChildren(self.points) > -1
+
+    def isFilo(self, maxLength):
+        """A branch is considered a Filo if it has no children, not a lamella, and not too long.
+
+        :returns: Tuple pair (whether it is a Filo, total length of the branch)"""
+        # If it has children, it's not a filo
+        if self.hasChildren():
+            return False, 0
+        # If it has a lamella, it's not a filo
+        if _lastPointWithLabel(self.points, 'lam') > -1:
+            return False, 0
+        totalLength, _ = self.worldLengths()
+        return totalLength < maxLength, totalLength
+
     def addPoint(self, point):
+        """Appends a point at the end of the branch.
+
+        :returns: the index of the new point."""
         self.points.append(point)
         point.parentBranch = self
         return len(self.points) - 1
 
     def insertPointBefore(self, point, index):
+        """Appends a point within a branch.
+
+        :returns: the index of the new point."""
         self.points.insert(index, point)
         point.parentBranch = self
         return index
 
     def removePointLocally(self, point):
+        """Remove a single point from the branch, leaving points before and after.
+
+        :returns: The point before this one"""
         if point not in self.points:
             print ("Deleting point not in the branch? Whoops")
-            return
+            return1
         index = self.points.index(point)
         self.points.remove(point)
         return self.parentPoint if index == 0 else self.points[index - 1]
 
     def setParentPoint(self, parentPoint):
+        """Sets the parent point for this branch, and adds the branch to its parent's children."""
         self.parentPoint = parentPoint
         self.parentPoint.children.append(self)
 
     def worldLengths(self):
+        """Returns world length of the branch, plus the length to the last branch point.
+
+        :returns: (totalLength, totalLength to last branch)
+        """
         pointsWithRoot = [self.parentPoint] + self.points
         x, y, z = self._parentTree.worldCoordPoints(pointsWithRoot)
         lastBranchPoint = _lastPointWithChildren(pointsWithRoot)
@@ -100,6 +135,9 @@ class Branch():
         return totalLength, totalLengthToLastBranch
 
     def cumulativeWorldLengths(self):
+        """Calculate the length to all points along the branch.
+
+        :returns: List of cumulative lengths, how far along the branch to get to each point."""
         pointsWithRoot = [self.parentPoint] + self.points
         x, y, z = self._parentTree.worldCoordPoints(pointsWithRoot)
         cumulativeLength, lengths = 0, []
@@ -108,19 +146,6 @@ class Branch():
             cumulativeLength += edgeDistance
             lengths.append(cumulativeLength)
         return lengths
-
-    def hasChildren(self):
-        return _lastPointWithChildren(self.points) > -1
-
-    def isFilo(self, maxLength):
-        # If it has children, it's not a filo
-        if self.hasChildren():
-            return False, 0
-        # If it has a lamella, it's not a filo
-        if _lastPointWithLabel(self.points, 'lam') > -1:
-            return False, 0
-        totalLength, _ = self.worldLengths()
-        return totalLength < maxLength, totalLength
 
 @attr.s
 class Tree():
@@ -134,23 +159,29 @@ class Tree():
 
     # HACK - make faster, index points by ID
     def getPointByID(self, pointID):
+        """Given the ID of a point, find the point object that matches."""
         for point in self.flattenPoints():
             if point.id == pointID:
                 return point
         return None
 
     def getBranchByID(self, branchID):
+        """Given the ID of a branch, find the branch object that matches."""
         for branch in self.branches:
             if branch.id == branchID:
                 return branch
         return None
 
     def addBranch(self, branch):
+        """Adds a branch to the tree.
+
+        :returns: Index of branch within the tree."""
         self.branches.append(branch)
         branch._parentTree = self
         return len(self.branches) - 1
 
     def removeBranch(self, branch):
+        """Removes a branch from the tree - assumes all points already removed."""
         if branch not in self.branches:
             print ("Deleting branch not in the tree? Whoops")
             return
@@ -160,13 +191,20 @@ class Tree():
         self.branches.remove(branch)
 
     def removePointByID(self, pointID):
+        """Removes a single point from the tree, identified by ID."""
         pointToRemove = self.getPointByID(pointID)
         if pointToRemove is not None:
             return pointToRemove.parentBranch.removePointLocally(pointToRemove)
         else:
             return None
 
-    def movePoint(self, pointID, newLocation, moveDownstream):
+    def movePoint(self, pointID, newLocation, moveDownstream=False):
+        """Moves a point to a new loction, optionally also moving all downstream points by the same.
+
+        :param pointID: ID of point to move.
+        :param newLocation: (x, y, z) tuple to move it to.
+        :param moveDownstream: Boolean flag of whether to move all child points and points later in the branch.
+        """
         pointToMove = self.getPointByID(pointID)
         assert pointToMove is not None, "Trying to move an unknown point ID"
         delta = util.locationMinus(newLocation, pointToMove.location)
@@ -176,7 +214,41 @@ class Tree():
             # Non-recursive, so just move this one point:
             pointToMove.location = newLocation
 
+    def flattenPoints(self):
+        """Returns all points in the tree, as a single list."""
+        if self.rootPoint is None:
+            return []
+        result = [self.rootPoint]
+        for branch in self.branches:
+            result.extend(branch.points)
+        return result
+
+    def closestPointTo(self, targetLocation, zFilter=False):
+        """Given a position, find the point closest to it.
+
+        :param targetLocation: (x, y, z) location tuple.
+        :param zFilter: If true, only items on the same zStack are considered.
+        :returns: Point object of point closest to the target location."""
+        # TODO - need to fix for world coordinates?
+        closestDist, closestPoint = None, None
+        for point in self.flattenPoints():
+            if zFilter and point.location[2] != targetLocation[2]:
+                continue
+            dist = util.deltaSz(targetLocation, point.location)
+            if closestDist is None or dist < closestDist:
+                closestDist, closestPoint = dist, point
+        return closestPoint
+
+    def worldCoordPoints(self, points):
+        """Convert image pixel (x, y, z) to a real-world (x, y, z) position by scaling."""
+        SCALE = [0.3070, 0.3070, 1.5] # HACK: Store in state instead
+        x = [p.location[0] * SCALE[0] for p in points]
+        y = [p.location[1] * SCALE[1] for p in points]
+        z = [p.location[2] * SCALE[2] for p in points]
+        return x, y, z
+
     def _recursiveMovePointDelta(self, point, delta):
+        """Recursively move a point, plus all its children and later neighbours."""
         point.location = util.locationPlus(point.location, delta)
         # First, move any branches coming off this point, by moving their first point
         for branch in self.branches:
@@ -189,31 +261,6 @@ class Tree():
             if nextIdx < len(point.parentBranch.points):
                 self._recursiveMovePointDelta(point.parentBranch.points[nextIdx], delta)
 
-    def flattenPoints(self):
-        if self.rootPoint is None:
-            return []
-        result = [self.rootPoint]
-        for branch in self.branches:
-            result.extend(branch.points)
-        return result
-
-    def closestPointTo(self, targetLocation, zFilter=False):
-        # TODO - need to fix for world coordinates?
-        closestDist, closestPoint = None, None
-        for point in self.flattenPoints():
-            if zFilter and point.location[2] != targetLocation[2]:
-                continue
-            dist = util.deltaSz(targetLocation, point.location)
-            if closestDist is None or dist < closestDist:
-                closestDist, closestPoint = dist, point
-        return closestPoint
-
-    def worldCoordPoints(self, points):
-        SCALE = [0.3070, 0.3070, 1.5] # HACK: Store in state instead
-        x = [p.location[0] * SCALE[0] for p in points]
-        y = [p.location[1] * SCALE[1] for p in points]
-        z = [p.location[2] * SCALE[2] for p in points]
-        return x, y, z
 
 ### Branch utilities
 
