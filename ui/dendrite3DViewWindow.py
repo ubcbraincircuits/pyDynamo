323aa1d4475e26f962c45d4c7bd1700a59f7de2c
padster <padsterpat@gmail.com>
2018-08-29 11:59:21 -0700

    Add the ability to save 2D/3D plots to file

--- ui/dendrite3DViewWindow.py
+++ ui/dendrite3DViewWindow.py
@@ -12,12 +12,20 @@ class Dendrite3DViewWindow(QtWidgets.QMainWindow):
         self.setWindowTitle('3D Dendrite view for ' + imagePath)
 
         root = QtWidgets.QWidget(self)
-        view3D = Dendrite3DCanvas(treeModel)
+        self.view3D = Dendrite3DCanvas(treeModel)
 
         # Assemble the view hierarchy.
         l = QtWidgets.QVBoxLayout(root)
         l.setContentsMargins(0, 0, 0, 0)
-        l.addWidget(view3D)
+        l.addWidget(self.view3D)
         root.setFocus()
         self.setCentralWidget(root)
         self.resize(1024, 768)
+
+        # Set up menu
+        fileMenu = QtWidgets.QMenu('File', self)
+        fileMenu.addAction('Save images to .png...', self.saveImagesAsPng, QtCore.Qt.CTRL + QtCore.Qt.Key_S)
+        self.menuBar().addMenu(fileMenu)
+
+    def saveImagesAsPng(self):
+        self.view3D.saveAxesAsPng()
