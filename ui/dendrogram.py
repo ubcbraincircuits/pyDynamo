a5d6dc47983195a46697a484a18dd53de8d88683
padster <padsterpat@gmail.com>
2018-08-29 12:28:50 -0700

    Update dendrogram layout to maintain left/right sides

--- ui/dendrogram.py
+++ ui/dendrogram.py
@@ -5,12 +5,14 @@ from analysis import addedSubtractedTransitioned
 from model import FiloType
 import util
 
+FILO_WIDTH_SCALE = 0.8 # Max visual width of a filo, where 1.0 = full column width
+
 def _fillWithOffset(toMap, fromMap, offset):
     for id, value in fromMap.items():
         toMap[id] = value + offset
 
 # Return a mapping of point ID -> X position, for all points downstream from a branch.
-def _pointXFromBranch(branch, branchIsFiloMap, filoDist):
+def _pointXFromBranch(branch, branchIsFiloMap, branchDrawnLeft, filoDist):
     localPointX = {}
 
     # Child branches, in eventual order of being drawn left to right:
@@ -25,26 +27,40 @@ def _pointXFromBranch(branch, branchIsFiloMap, filoDist):
                 childFilo.append(childBranch)
             else:
                 # Alternate whether child branches are shown on the left or right:
-                if len(childBranchesLeft) == len(childBranchesRight):
+                forceLeft, forceRight = False, False
+                if childBranch.id in branchDrawnLeft:
+                    forceLeft = branchDrawnLeft[childBranch.id]
+                    forceRight = not branchDrawnLeft[childBranch.id]
+                preferLeft = len(childBranchesLeft) <= len(childBranchesRight)
+                print (forceLeft, forceRight, preferLeft)
+                if forceLeft or (preferLeft and not forceRight):
                     childBranchesLeft = childBranchesLeft + [childBranch]
+                    branchDrawnLeft[childBranch.id] = True
                 else:
                     childBranchesRight = [childBranch] + childBranchesRight
+                    branchDrawnLeft[childBranch.id] = False
 
     # Place all pointsin branches coming out the left in order...
     atX = 0
     for leftBranch in childBranchesLeft:
-        childPointX = _pointXFromBranch(leftBranch, branchIsFiloMap, filoDist)
+        childPointX = _pointXFromBranch(leftBranch, branchIsFiloMap, branchDrawnLeft, filoDist)
         if len(childPointX) > 0:
             _fillWithOffset(localPointX, childPointX, atX)
             atX += math.floor(max(childPointX.values())) + 1
 
     # ... then place filopodia points sideways based off length...
-    nextOffset = -1
+    nextIsLeft = -1
     for filo in childFilo:
+        if filo.id in branchDrawnLeft:
+            nextIsLeft = branchDrawnLeft[filo.id]
+        else:
+            branchDrawnLeft[filo.id] = nextIsLeft
+
         lengths = filo.cumulativeWorldLengths()
         for point, dist in zip(filo.points, lengths):
-            localPointX[point.id] = atX + (dist/filoDist) * nextOffset * 0.9
-        nextOffset *= -1 # alternate on left vs right.
+            offset = -1 if nextIsLeft else 1
+            localPointX[point.id] = atX + (dist/filoDist) * offset * FILO_WIDTH_SCALE
+        nextIsLeft = not nextIsLeft # alternate on left vs right.
 
     # ... then place all the points on this branch in the center...
     for point in branch.points:
@@ -53,7 +69,7 @@ def _pointXFromBranch(branch, branchIsFiloMap, filoDist):
 
     # ... and finally place points in branches coming off to the right.
     for rightBranch in childBranchesRight:
-        childPointX = _pointXFromBranch(rightBranch, branchIsFiloMap, filoDist)
+        childPointX = _pointXFromBranch(rightBranch, branchIsFiloMap, branchDrawnLeft, filoDist)
         if len(childPointX) > 0:
             _fillWithOffset(localPointX, childPointX, atX)
             atX += math.floor(max(childPointX.values())) + 1
@@ -62,7 +78,7 @@ def _pointXFromBranch(branch, branchIsFiloMap, filoDist):
 
 
 # Return a mapping of point ID -> X position, for all points in a tree.
-def _calculatePointX(tree, branchIsFiloMap, filoDist):
+def _calculatePointX(tree, branchIsFiloMap, branchDrawnLeft, filoDist):
     assert tree.rootPoint is not None
     pointX = {}
 
@@ -71,7 +87,7 @@ def _calculatePointX(tree, branchIsFiloMap, filoDist):
 
     for branch in tree.rootPoint.children:
         # Get X for our child...
-        childPointX = _pointXFromBranch(branch, branchIsFiloMap, filoDist)
+        childPointX = _pointXFromBranch(branch, branchIsFiloMap, branchDrawnLeft, filoDist)
         if len(childPointX) > 0:
             # ...and offset by how far we are:
             _fillWithOffset(pointX, childPointX, atX)
@@ -96,10 +112,12 @@ def _pointYFromBranch(collector, branchIsFiloMap, branch, yAt):
 
     # Otherwise, draw points upwards based of world length.
     lengths = branch.cumulativeWorldLengths()
+    firstScale = 0 # Make the first one horizontal, not higher.
     for point, dist in zip(branch.points, lengths):
-        collector[point.id] = yAt + dist
+        collector[point.id] = yAt + dist * firstScale
+        firstScale = 1
         for childBranch in point.children:
-            _pointYFromBranch(collector, branchIsFiloMap, childBranch, yAt + dist)
+            _pointYFromBranch(collector, branchIsFiloMap, childBranch, collector[point.id])
 
 
 # Return a mapping of point ID -> Y position, for all points in a tree.
@@ -113,7 +131,10 @@ def _calculatePointY(tree, branchIsFiloMap):
 
 
 # Calculate X and Y positions for all points within a tree.
-def calculatePositions(tree, branchIDToFiloTypeMap=None, filoDist=10):
+def calculatePositions(tree, branchIDToFiloTypeMap=None, filoDist=10, branchDrawnLeft=None):
+    if branchDrawnLeft is None:
+        branchDrawnLeft = {} # No directions stored yet.
+
     # First calculate branch types if it hasn't been done yet:
     if branchIDToFiloTypeMap is None:
         ftArray, _, _ , _, _, _ = addedSubtractedTransitioned([tree], filoDist=filoDist)
@@ -129,17 +150,18 @@ def calculatePositions(tree, branchIDToFiloTypeMap=None, filoDist=10):
         branchIsFiloMap[id] = (filoType == FiloType.INTERSTITIAL or filoType == FiloType.TERMINAL)
 
     # Finally, calculate both X and Y positions
-    pointX = _calculatePointX(tree, branchIsFiloMap, filoDist)
+    pointX = _calculatePointX(tree, branchIsFiloMap, branchDrawnLeft, filoDist)
     pointY = _calculatePointY(tree, branchIsFiloMap)
     return pointX, pointY
 
 def calculateAllPositions(trees, filoTypes, branchIDList, filoDist=10):
     allX, allY = [], []
+    branchDrawnLeft = {}
     for tree, treeFiloTypes in zip(trees, filoTypes):
         branchIDToFiloTypeMap = {}
         for i, id in enumerate(branchIDList):
             branchIDToFiloTypeMap[id] = treeFiloTypes[i]
-        treeX, treeY = calculatePositions(tree, branchIDToFiloTypeMap, filoDist)
+        treeX, treeY = calculatePositions(tree, branchIDToFiloTypeMap, filoDist, branchDrawnLeft)
         allX.append(treeX)
         allY.append(treeY)
     return allX, allY
