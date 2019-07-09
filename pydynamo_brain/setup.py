75c8d680b3d0fff68b3392279c9eb56c56f28e9b
padster <padsterpat@gmail.com>
2019-07-09 12:23:46 -0700

    Fixed up hyphen to underscore

--- pydynamo_brain/setup.py
+++ pydynamo_brain/setup.py
@@ -35,7 +35,7 @@ setup(
     # There are some restrictions on what makes a valid project name
     # specification here:
     # https://packaging.python.org/specifications/core-metadata/#name
-    name='pydynamo-brain',  # Required
+    name='pydynamo_brain',  # Required
 
     # Versions should comply with PEP 440:
     # https://www.python.org/dev/peps/pep-0440/
