441c850883a3e30f62da815d3f7ae340ab06de9d
Pat Coleman <padsterpat@gmail.com>
2019-07-31 10:40:26 -0700

    Expose parseMatlabTree for scripts

--- pydynamo_brain/pydynamo_brain/files/__init__.py
+++ pydynamo_brain/pydynamo_brain/files/__init__.py
@@ -1,5 +1,5 @@
 from .autosaver import AutoSaver
 from .files import loadState, saveState, checkIfChanged, fullStateToString, stringToFullState
-from .matlab import importFromMatlab
+from .matlab import importFromMatlab, parseMatlabTree
 from .swc import exportToSWC, importFromSWC
 from .idremap import saveRemapWithMerge
