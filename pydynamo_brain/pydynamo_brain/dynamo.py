4b9167723f82bbecc81467324fc46bb9ff8cdabc
Pat Coleman <padsterpat@gmail.com>
2019-08-01 14:06:58 -0700

    Get mpl using QT only when running dynamo, not when imported

--- pydynamo_brain/pydynamo_brain/dynamo.py
+++ pydynamo_brain/pydynamo_brain/dynamo.py
@@ -2,11 +2,13 @@ import sys
 import matplotlib
 from PyQt5 import QtCore, QtWidgets
 
-matplotlib.use('Qt5Agg')
-from mpl_toolkits.mplot3d import Axes3D
 from .ui import DynamoWindow
 
 def runDynamo():
+    # Need to configure mpl before importing 3D axes
+    matplotlib.use('Qt5Agg')
+    from mpl_toolkits.mplot3d import Axes3D
+
     qtApp = QtWidgets.QApplication(sys.argv)
     dynamoWindow = DynamoWindow(qtApp, sys.argv[1:])
     print ("Exec!")
