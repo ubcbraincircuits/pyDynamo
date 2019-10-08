29d6ebbb01e6af97976ee8fc4e8292fa150f0d59
padster <padsterpat@gmail.com>
2019-10-08 16:38:47 -0700

    Add scaling for xyz dimensions in 3D view

--- pydynamo_brain/pydynamo_brain/ui/volume3DWindow.py
+++ pydynamo_brain/pydynamo_brain/ui/volume3DWindow.py
@@ -12,11 +12,21 @@ class Volume3DWindow():
 
         # Load in the volume to show, picking one channel:
         volume = _IMG_CACHE.getVolume(self.uiState.imagePath)
+
+        # TODO: Add one layer per channel
         self.data = volume[self.uiState._parent.channel]
+        print (self.data.shape)
 
     def show(self):
         with napari.gui_qt():
             viewer = napari.view_image(self.data, name='Volume', rgb=False)
+            xyzScale = self.uiState._parent.projectOptions.pixelSizes
+            zyxScale = [1, xyzScale[1] / xyzScale[2], xyzScale[0] / xyzScale[2]]
+            viewer.layers['Volume'].scale = zyxScale
+
             viewer.layers['Volume'].contrast_limits = [cl * 255 for cl in self.uiState.colorLimits]
             viewer.dims.ndim = 3
             viewer.dims.ndisplay = 3
+
+            # TODO: Add shape layer for drawing
+            # viewer.layers['Shapes'].add(y, shape_type='rectangle')
