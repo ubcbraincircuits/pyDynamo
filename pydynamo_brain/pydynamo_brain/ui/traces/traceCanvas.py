fda4593da950e98d4fc4673aa3746b60b1062ff5
padster <padsterpat@gmail.com>
2020-12-31 13:24:23 -0800

    Filters and Split working

--- pydynamo_brain/pydynamo_brain/ui/traces/traceCanvas.py
+++ pydynamo_brain/pydynamo_brain/ui/traces/traceCanvas.py
@@ -1,11 +1,20 @@
 import numpy as np
 
+from matplotlib.ticker import FuncFormatter
+
 from pydynamo_brain.files import TraceCache
 from pydynamo_brain.ui.baseMatplotlibCanvas import BaseMatplotlibCanvas
 
 import pydynamo_brain.util as util
 
 _TRACE_CACHE = TraceCache()
+
+# Utility to pad (min, max) range by a small amount and return the new ones
+def _addPad(lo, hi, pad):
+    mid = (lo + hi) / 2
+    haf = hi - mid
+    return mid - (1 + pad) * haf, mid + (1 + pad) * haf
+
 
 # Draws a dendritic tree in 3D space that can be rotated by the user.
 class TraceCanvas(BaseMatplotlibCanvas):
@@ -14,47 +23,82 @@ class TraceCanvas(BaseMatplotlibCanvas):
         self.pointsToShow = [] # Order matters
         self.tracesToShow = []
         self.split = True
+        self.traceViewWindow = parent
 
-        super(TraceCanvas, self).__init__(*args, in3D=False, **kwargs)
+        super(TraceCanvas, self).__init__(parent, *args, in3D=False, **kwargs)
 
-        self.axes[0].patch.set_facecolor('black')
-        vPad, hPad = 0.05, 0.1
+        vPad, hPad = 0.1, 0.15
         self.fig.subplots_adjust(top=1.0-vPad, bottom=vPad, right=1.0-hPad, left=hPad)
 
     def compute_initial_figure(self):
         # Initial figure, also used on update.
         ax = self.axes[0]
+        ax.patch.set_facecolor('black')
 
         self.tracesToShow = []
         for (wID, pID) in self.pointsToShow:
-            print ("Point %s in window %d" % (str(pID), wID))
-
             trace = None
             if wID < len(self.fullState.traces):
                 tracePaths = self.fullState.traces[wID]
-                trace = _TRACE_CACHE.getTraceForPOI(tracePaths, pID, verbose=True)
+                trace = _TRACE_CACHE.getTraceForPOI(tracePaths, pID, verbose=False)
 
             if trace is not None:
-                self.tracesToShow.append((pID, wID, trace))
+                data, hz = trace.data, trace.rate
+                samples = self.traceViewWindow.applyFilters(data, hz)
+                self.tracesToShow.append((pID, wID, samples, hz))
             else:
-                print ("  -> No trace! Skipping")
+                # print ("  -> No trace! Skipping")
+                pass
 
         if len(self.tracesToShow) == 0:
             return
+
+        ax.axvline(0, ls=':', c=(1, 1, 1, 1), lw=0.5)
 
         maxV = np.max([np.max(t[2].data) for t in self.tracesToShow])
         minV = np.min([np.min(t[2].data) for t in self.tracesToShow])
 
         perLineOffset = 0 if not self.split else maxV - minV
 
-        for i, (pID, wID, trace) in enumerate(self.tracesToShow):
-            data, hz = trace.data, trace.rate
-            x = np.arange(len(data)) * (1 / hz) # Samples -> Seconds
-            y = data
+        minX, maxX, minY, maxY = None, None, None, None
+
+        yZeros = []
+        for i, (pID, wID, samples, hz) in enumerate(self.tracesToShow):
+            x = np.arange(len(samples)) * (1 / hz) # Samples -> Seconds
+            y = samples
             off = len(self.tracesToShow) - 1 - i
-            ax.plot(x, y + off * perLineOffset, label="%s @ %s" % (pID, wID))
-            if i == 0 or self.split:
-                ax.axhline(off * perLineOffset, ls=':', c='k')
+            yV = y + off * perLineOffset
+            ax.plot(x, yV, label="%s @ %s" % (pID, wID))
+
+            if minX is None:
+                minX, maxX = np.min(x), np.max(x)
+                minY, maxY = np.min(yV), np.max(yV)
+            else:
+                minX, maxX = min(minX, np.min(x)), max(maxX, np.max(x))
+                minY, maxY = min(minY, np.min(yV)), max(maxY, np.max(yV))
+
+            if off == 0 or self.split:
+                ax.axhline(off * perLineOffset, ls=':', c=(1, 1, 1, 0.5), lw=1)
+                yZeros.append(off * perLineOffset)
+        yZeros = np.array(yZeros)
+
+
+        # X axis is time
+        ax.get_xaxis().set_major_formatter(FuncFormatter(lambda x, pos: "%.2fs" % x))
+        ax.set_xlabel("Time (sec)")
+        ax.set_xlim(*_addPad(minX, maxX, 0.05))
+
+        # Y axis is value, possibly split
+        yTickScale = int(0.7 * maxV)
+        yTickPct = self.traceViewWindow.showPercent()
+        tickFmt = "%d%%" if yTickPct else "%.2f"
+        zeroText, deltaText = tickFmt % (0), tickFmt % (yTickScale * (100 if yTickPct else 1))
+        ax.set_yticks(yZeros, minor=False)
+        ax.set_yticks(yZeros + yTickScale, minor=True)
+        ax.get_yaxis().set_major_formatter(FuncFormatter(lambda x, pos: zeroText))
+        ax.get_yaxis().set_minor_formatter(FuncFormatter(lambda x, pos: deltaText))
+        ax.set_ylabel(self.traceViewWindow.getYLabel())
+        ax.set_ylim(*_addPad(minY, maxY, 0.05))
 
         ax.legend()
 
--- pydynamo_brain/pydynamo_brain/ui/traces/traceViewWindow.py
+++ pydynamo_brain/pydynamo_brain/ui/traces/traceViewWindow.py
@@ -1,5 +1,8 @@
 from PyQt5 import QtCore, QtWidgets
 
+import numpy as np
+import pyneurotrace.filters as filters
+
 from .traceCanvas import TraceCanvas
 
 # HACK: Global editable
@@ -27,17 +30,40 @@ class TraceViewWindow(QtWidgets.QMainWindow):
         root.setFocus()
         self.setCentralWidget(root)
 
-        # TODO: Menu options for DF/F0, okada and smooth filters.
-        #viewMenu = QtWidgets.QMenu('View', self)
-        #viewMenu.addAction('Show 3D', self.show3D, QtCore.Qt.Key_3)
-        #viewMenu.addAction('Show 2D Dendrograms', self.show2D, QtCore.Qt.Key_2)
-        #viewMenu.addSeparator()
-        #self._addScrollActions(viewMenu)
-        #self.menuBar().addMenu(viewMenu)
+        viewMenu = QtWidgets.QMenu('View', self)
+        viewMenu.addAction('Split traces', self.toggleSplit, QtCore.Qt.Key_S)
+        self.menuBar().addMenu(viewMenu)
+
+        filterMenu = QtWidgets.QMenu('Filters', self)
+        self.okadaToggle = QtWidgets.QAction('Okada', filterMenu, checkable=True, checked=True)
+        self.dfof0Toggle = QtWidgets.QAction('DF/F0', filterMenu, checkable=True, checked=True)
+        self.nndSmoothToggle = QtWidgets.QAction('NND Smooth', filterMenu, checkable=True, checked=False)
+        for toggle in [self.okadaToggle, self.dfof0Toggle, self.nndSmoothToggle]:
+            filterMenu.addAction(toggle)
+            toggle.triggered.connect(self._filtersChangeEvent)
+        self.menuBar().addMenu(filterMenu)
+
 
     def closeEvent(self, event):
         event.accept()
         _INSTANCE[0] = None
+
+    def _filtersChangeEvent(self):
+        self.view.needToUpdate()
+
+    def applyFilters(self, trace, hz):
+        # These filters assume multiple rows, make into a single row:
+        trace = np.expand_dims(trace, axis=0)
+
+        # TODO: support parameters for these in project options?
+        if self.okadaToggle.isChecked():
+            trace = filters.okada(trace)
+        if self.dfof0Toggle.isChecked():
+            trace = filters.deltaFOverF0(trace, int(hz))
+        if self.nndSmoothToggle.isChecked():
+            trace = filters.nndSmooth(trace, hz, tau=0.75)
+
+        return trace[0] # Extract back single row.
 
     def togglePointInWindow(self, windowIndex, pointID):
         isEmpty = self.view.togglePointInWindow(windowIndex, pointID)
@@ -46,3 +72,15 @@ class TraceViewWindow(QtWidgets.QMainWindow):
         else:
             # TODO: Bring to front? Maybe not, if more convenient.
             pass
+
+    def toggleSplit(self):
+        self.view.split = not self.view.split
+        self.view.needToUpdate()
+
+    def showPercent(self):
+        return self.dfof0Toggle.isChecked()
+
+    def getYLabel(self):
+        prefix = "Smoothed " if self.nndSmoothToggle.isChecked() else ""
+        suffix = "DF/F0 (%)" if self.dfof0Toggle.isChecked() else "Raw"
+        return prefix + suffix
