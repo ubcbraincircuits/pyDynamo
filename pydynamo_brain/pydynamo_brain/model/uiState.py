674b5a74d15a5a029d74427f3a3b88b1050341af
Peter-Hogg <peterhogg2006@gmail.com>
2023-11-05 17:08:47 -0800

    Fixed a bug with point deletion

--- pydynamo_brain/pydynamo_brain/model/uiState.py
+++ pydynamo_brain/pydynamo_brain/model/uiState.py
@@ -306,9 +306,10 @@ class UIState():
             return None
 
         if pointID != self._tree.rootPoint.id:
-            for branch in self._tree.branches:
-                if branch.parentPoint is not None and branch.parentPoint.id == pointID:
-                    self.deleteBranch(branch)
+            if len(self._tree.getPointByID(pointID).children) > 0:
+                for child in self._tree.getPointByID(pointID).children:
+                    self.deleteBranch(child)
+            
         return self._tree.removePointByID(pointID)
 
     def endMove(self, newLocation: Point3D, downstream: bool) -> None:
